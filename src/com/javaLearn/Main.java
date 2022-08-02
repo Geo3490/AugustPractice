1434ac788731ab331a31fdfd10b615f2f71ea65a
Geo3490 <geo40127@gmail.com>
2022-08-02 18:48:06 -0400

    GUI

--- src/com/javaLearn/Main.java
+++ src/com/javaLearn/Main.java
@@ -1,9 +1,35 @@
 package com.javaLearn;
 
+import javax.swing.JPanel;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+
 public class Main {
 
     public static void main(String[] args) {
-	EmployeeData employee = new EmployeeData("George", 32, 'd');
+
+        JPanel panel = new JPanel();
+        JFrame frame = new JFrame();
+
+
+
+
+        frame.setSize(350, 200);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.add(panel);
+
+        panel.setLayout(null);
+
+        JLabel label = new JLabel("User");
+        label.setBounds(10,20,80,25);
+        panel.add(label);
+
+        JTextField textField = new JTextField(20);
+        panel.add(textField);
+
+        frame.setVisible(true);
 
     }
 }
